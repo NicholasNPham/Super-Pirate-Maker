9fc75362b5b68f2d0f88a65378bce205ea79cdb4
Nicholas Pham <132528074+NicholasNPham@users.noreply.github.com>
2023-06-28 19:45:34 -0400

    Add files via upload

--- code/editor.py
+++ code/editor.py
@@ -11,6 +11,7 @@ class Editor:
 
         # Main Setup
         self.display_surface = pygame.display.get_surface()
+        self.canvas_data = {}
 
         # Navigation
         self.origin = vector()
@@ -24,9 +25,26 @@ class Editor:
 
         # Selection
         self.selection_index = 2
+        self.last_selected_cell = None
 
         # Menu
         self.menu = Menu()
+
+    # Support
+    def get_current_cell(self):
+        distance_to_origin = vector(mouse_pos()) - self.origin
+
+        if distance_to_origin.x > 0:
+            col = int(distance_to_origin.x / TILE_SIZE)
+        else:
+            col = int(distance_to_origin.x / TILE_SIZE) -1
+
+        if distance_to_origin.y > 0:
+            row = int(distance_to_origin.y / TILE_SIZE)
+        else:
+            row = int(distance_to_origin.y / TILE_SIZE) -1
+
+        return col, row
 
     # Input
     def event_loop(self):
@@ -39,6 +57,7 @@ class Editor:
             self.pan_input(event)
             self.selection_hotkeys(event)
             self.menu_click(event)
+            self.canvas_add()
 
     def pan_input(self, event):
 
@@ -73,6 +92,22 @@ class Editor:
         if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
             self.selection_index = self.menu.click(mouse_pos(), mouse_buttons())
 
+    def canvas_add(self):
+        if mouse_buttons()[0] and not self.menu.rect.collidepoint(mouse_pos()):
+            current_cell = self.get_current_cell()
+
+            if current_cell != self.last_selected_cell:
+
+                if current_cell in self.canvas_data:
+                    self.canvas_data[current_cell].add_id(self.selection_index)
+                else:
+                    self.canvas_data[current_cell] = CanvasTile(self.selection_index)
+
+                self.last_selected_cell = current_cell
+
+        for key, value in self.canvas_data.items():
+            print(f'{key}: {value.has_terrain}')
+
     # Drawing
     def draw_tiles_lines(self):
         cols = WINDOW_WIDTH // TILE_SIZE
@@ -95,11 +130,71 @@ class Editor:
 
         self.display_surface.blit(self.support_line_surf, (0, 0))
 
+    def draw_level(self):
+        for cell_pos, tile in self.canvas_data.items():
+            pos = self.origin + vector(cell_pos) * TILE_SIZE
+
+            # Water
+            if tile.has_water:
+                test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+                test_surf.fill('blue')
+                self.display_surface.blit(test_surf, pos)
+
+            # Terrain
+            if tile.has_terrain:
+                test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+                test_surf.fill('brown')
+                self.display_surface.blit(test_surf, pos)
+
+            # Coin
+            if tile.coin:
+                test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+                test_surf.fill('yellow')
+                self.display_surface.blit(test_surf, pos)
+
+            # Enemy
+            if tile.enemy:
+                test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+                test_surf.fill('black')
+                self.display_surface.blit(test_surf, pos)
+
+    # Update
     def run(self, dt):
         self.event_loop()
 
         # Drawing
         self.display_surface.fill('white')
+        self.draw_level()
         self.draw_tiles_lines()
         pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
         self.menu.display(self.selection_index)
+
+class CanvasTile:
+    def __init__(self, tile_id):
+
+        # Terrain
+        self.has_terrain = False
+        self.terrain_neighbors = []
+
+        # Water
+        self.has_water = False
+        self.water_on_top = False
+
+        # Coin
+        self.coin = None
+
+        # Enemy
+        self.enemy = None
+
+        # Objects
+        self.objects = []
+
+        self.add_id(tile_id)
+
+    def add_id(self, tile_id):
+        options = {key: value['style'] for key, value in EDITOR_DATA.items()}
+        match options[tile_id]:
+            case 'terrain': self.has_terrain = True
+            case 'water': self.has_water = True
+            case 'coin': self.coin = tile_id
+            case 'enemy': self.enemy = tile_id
