8671dc11b25ae809c06f4b0f945104df2744fbe1
Nicholas Pham <132528074+NicholasNPham@users.noreply.github.com>
2023-07-15 01:47:57 -0400

    Add files via upload

--- code/level.py
+++ code/level.py
@@ -4,7 +4,7 @@ from pygame.math import Vector2 as vector
 from settings import *
 from support import *
 
-from sprites import Generic, Animated, Particle, Coin, Player, Spikes, Tooth, Shell
+from sprites import Generic, Block, Animated, Particle, Coin, Player, Spikes, Tooth, Shell
 
 class Level:
     def __init__(self, grid, switch, asset_dict):
@@ -15,6 +15,7 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
         self.coin_sprites = pygame.sprite.Group()
         self.damage_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         self.build_level(grid, asset_dict)
 
@@ -25,7 +26,7 @@ class Level:
         for layer_name, layer in grid.items():
             for pos, data in layer.items():
                 if layer_name == 'terrain':
-                    Generic(pos, asset_dict['land'][data], self.all_sprites)
+                    Generic(pos, asset_dict['land'][data], [self.all_sprites, self.collision_sprites])
                 if layer_name == 'water':
                     if data == 'top':
                         Animated(asset_dict['water top'], pos, self.all_sprites)
@@ -33,7 +34,7 @@ class Level:
                         Generic(pos, asset_dict['water bottom'], self.all_sprites)
 
                 match data:
-                    case 0: self.player = Player(pos, self.all_sprites)
+                    case 0: self.player = Player(pos, self.all_sprites, self.collision_sprites)
 
                     # Coins
                     case 4: Coin('gold', asset_dict['gold'], pos, [self.all_sprites, self.coin_sprites])
@@ -43,19 +44,27 @@ class Level:
                     # Enemies
                     case 7: Spikes(asset_dict['spikes'], pos, [self.all_sprites, self.damage_sprites]) # Spikes
                     case 8: Tooth(asset_dict['tooth'], pos, [self.all_sprites, self.damage_sprites])
-                    case 9: Shell("left", asset_dict['shell'], pos, self.all_sprites)
-                    case 10: Shell("right", asset_dict['shell'], pos, self.all_sprites)
+                    case 9: Shell("left", asset_dict['shell'], pos, [self.all_sprites, self.collision_sprites])
+                    case 10: Shell("right", asset_dict['shell'], pos, [self.all_sprites, self.collision_sprites])
 
                     # Palm Trees
-                    case 11: Animated(asset_dict['palms']['small_fg'], pos, self.all_sprites)
-                    case 12: Animated(asset_dict['palms']['large_fg'], pos, self.all_sprites)
-                    case 13: Animated(asset_dict['palms']['left_fg'], pos, self.all_sprites)
-                    case 14: Animated(asset_dict['palms']['right_fg'], pos, self.all_sprites)
+                    case 11:
+                        Animated(asset_dict['palms']['small_fg'], pos, self.all_sprites)
+                        Block(pos, (80, 50), self.collision_sprites)
+                    case 12:
+                        Animated(asset_dict['palms']['large_fg'], pos, self.all_sprites)
+                        Block(pos, (80, 50), self.collision_sprites)
+                    case 13:
+                        Animated(asset_dict['palms']['left_fg'], pos, self.all_sprites)
+                        Block(pos, (80, 50), self.collision_sprites)
+                    case 14:
+                        Animated(asset_dict['palms']['right_fg'], pos, self.all_sprites)
+                        Block(pos + vector(50,0), (80, 50), self.collision_sprites)
+
                     case 15: Animated(asset_dict['palms']['small_bg'], pos, self.all_sprites)
                     case 16: Animated(asset_dict['palms']['large_bg'], pos, self.all_sprites)
                     case 17: Animated(asset_dict['palms']['left_bg'], pos, self.all_sprites)
                     case 18: Animated(asset_dict['palms']['right_bg'], pos, self.all_sprites)
-
 
     def get_coins(self):
         collided_coins = pygame.sprite.spritecollide(self.player, self.coin_sprites, True)
@@ -73,11 +82,13 @@ class Level:
     def run(self, dt):
         # Update
         self.event_loop()
-        self.display_surface.fill(SKY_COLOR)
+        self.all_sprites.update(dt)
         self.get_coins()
 
         # Drawing
-        self.all_sprites.update(dt)
+        self.display_surface.fill(SKY_COLOR)
         self.all_sprites.draw(self.display_surface)
+        pygame.draw.rect(self.display_surface, 'yellow', self.player.hitbox)
+
 
 
